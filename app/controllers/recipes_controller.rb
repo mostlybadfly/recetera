2e4902f8ca71d427bb3b740de82be6032ed273a3
mostlybadfly <reachdanielnunez@gmail.com>
2015-02-05 23:57:00 +0000

    Update Recipe form and controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,18 @@
 class RecipesController < ApplicationController
   def new
+    @recipe = Recipe.new
   end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.ingredients.build
+    @recipe.save
+    redirect_to :back
+  end
+
+  private
+    def recipe_params
+      params.require(:recipe).permit(:title, :instructions,
+                                     ingredients_attributes: [:name, :quantity])
+    end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,5 @@
 class Recipe < ActiveRecord::Base
   has_many :ingredients
+
+  accepts_nested_attributes_for :ingredients
 end
