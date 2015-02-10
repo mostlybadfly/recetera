b54d538575e2fe8a9cf20f01f8fd3e051635216e
mostlybadfly <reachdanielnunez@gmail.com>
2015-02-10 23:42:51 +0000

    Added 'show' view and redirects

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -19,7 +19,7 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
-      redirect_to recipes_path 
+      redirect_to @recipe 
     else
       render "new"
     end
@@ -29,7 +29,7 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
 
     if @recipe.update(recipe_params)
-      redirect_to recipes_path
+      redirect_to @recipe 
     else
       render "edit"
     end
