171719753ad8e94a55c3f4ac722dd7d99070fa72
mostlybadfly <reachdanielnunez@gmail.com>
2015-02-10 23:54:54 +0000

    Added #destroy and links

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -35,6 +35,13 @@ class RecipesController < ApplicationController
     end
   end
 
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    redirect_to recipes_path
+  end
+
   private
     def recipe_params
       params.require(:recipe).permit(:title, :instructions,
