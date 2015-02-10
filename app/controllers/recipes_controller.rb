d7efa8da9f8f8a44d86175ddf3c586a05d72454a
mostlybadfly <reachdanielnunez@gmail.com>
2015-02-10 17:51:36 +0000

    Added Update/Edit methods and view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,12 +8,26 @@ class RecipesController < ApplicationController
    @recipe.ingredients.build
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id]
+  end
+
   def create
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
       redirect_to recipes_path 
     else
       render "new"
+    end
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    if @recipe.update(recipe_params)
+      redirect_to recipes_path
+    else
+      render "edit"
     end
   end
 
