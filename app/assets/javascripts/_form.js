9fe7d5120dd2c7f10b11dc6b7c4de4be9be43b22
mostlybadfly <reachdanielnunez@gmail.com>
2015-02-18 18:58:16 +0000

    Added js functionality for instructions

---
+++ app/assets/javascripts/_form.js
@@ -0,0 +1,5 @@
+$(function() {
+  $("#new_step").on("click", function(){
+    $(".recipe_form").append('<input type="text" name="recipe[instructions][]">');
+  })
+});
--- app/assets/javascripts/new.js
+++
@@ -1,3 +0,0 @@
-$("#new_step").on("click", function(){
-    $("#recipe_form").append('<input type="text" name="recipe[instructions][]">');
-})
