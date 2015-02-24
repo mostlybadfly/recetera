2f9be957570ac596c933a0ff6414d0e1333facac
mostlybadfly <reachdanielnunez@gmail.com>
2015-02-24 00:32:18 +0000

    Fix new instruction fields

--- app/assets/javascripts/_form.js
+++ app/assets/javascripts/_form.js
@@ -1,5 +1,9 @@
-$(function() {
-  $("#new_step").on("click", function(){
-    $(".recipe_form").append('<input type="text" name="recipe[instructions][]">');
-  })
-});
+var ready;
+ready = function() {
+  $("#new_step").on("click", function() {
+    $(".rec_form").append('<input class="form-control" type="text" name="recipe[instructions][]">');
+  });
+};
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
