4c4ac86afc90a18beda58baf0c5e2d4504b28ed3
mostlybadfly <reachdanielnunez@gmail.com>
2015-02-24 16:29:25 +0000

    Updated styling in views

--- app/assets/javascripts/_form.js
+++ app/assets/javascripts/_form.js
@@ -1,7 +1,7 @@
 var ready;
 ready = function() {
   $("#new_step").on("click", function() {
-    $(".steps").append('<input class="form-control" type="text" name="recipe[instructions][]">');
+    $(".steps").append('<input class="form-control" type="text" name="recipe[instructions][]" placeholder="Type next step">');
   });
 };
 
