72bb7719840e4ae095ea9e30129f79a87bcb485d
mostlybadfly <reachdanielnunez@gmail.com>
2015-03-30 17:47:06 +0000

    Finish add/remove for Ingredients and instructions

--- app/assets/javascripts/_form.js
+++ app/assets/javascripts/_form.js
@@ -1,12 +1,12 @@
 var ready;
 ready = function() {
   $("#new_step").on("click", function(obj) {
-    $("#recipe_steps").append('<input class="new-step form-control" type="text" name="recipe[instructions][]" placeholder="Type next step">');
+    $("#recipe_steps").append('<input class="new-step form-control" type="text" name="recipe[instructions][]" placeholder="Type next step"><a href="javascript:void(0)" class="remove_step">Remove Step</a>');
   });
 
-  $("#remove_step").on("click", function() {
-    var list = document.getElementById("recipe_steps");
-    list.removeChild(list.lastChild);
+  $(document).on("click", ".remove_step", function() {
+    $(this).prev("input[type=text]").remove();
+    $(this).remove()
   });
 };
 
