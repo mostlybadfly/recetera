7ca9cd8d91835116ba9b1b245fec836743ee659d
mostlybadfly <reachdanielnunez@gmail.com>
2015-03-30 04:11:07 +0000

    Add and remove steps links

--- app/assets/javascripts/_form.js
+++ app/assets/javascripts/_form.js
@@ -1,11 +1,12 @@
 var ready;
 ready = function() {
   $("#new_step").on("click", function(obj) {
-    $(".steps").append('<input class="new-step form-control" type="text" name="recipe[instructions][]" placeholder="Type next step">');
+    $("#recipe_steps").append('<input class="new-step form-control" type="text" name="recipe[instructions][]" placeholder="Type next step">');
   });
 
   $("#remove_step").on("click", function() {
-    $(".new-step").remove();
+    var list = document.getElementById("recipe_steps");
+    list.removeChild(list.lastChild);
   });
 };
 
