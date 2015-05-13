010d0841b88c81e7db4fedf0966f14974aea945e
mostlybadfly <reachdanielnunez@gmail.com>
2015-05-13 23:17:30 +0000

    Update mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com"
+    port: "465"
+    domain: "gmail.com"
+    autentication: "plain"
+    user_name: ENV['EMAIL_USERNAME']
+    password: ENV['EMAIL_PASSWORD']
+    enable_starttls_auto: true
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
