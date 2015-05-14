4c226212465d3832d474b02257f3014b668bd1a6
mostlybadfly <reachdanielnunez@gmail.com>
2015-05-14 00:52:34 +0000

    update mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,12 +2,13 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'boiling-shelf-7458.herokuapp.com' }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "gmail.com",
+    domain: ENV['EMAIL_DOMAIN'],
     autentication: "plain",
     user_name: ENV['EMAIL_USERNAME'],
     password: ENV['EMAIL_PASSWORD'],
