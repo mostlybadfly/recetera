33f146a2f526868cc808dfed2a81d6e0f563886a
mostlybadfly <reachdanielnunez@gmail.com>
2015-05-14 00:32:22 +0000

    Update mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,8 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
